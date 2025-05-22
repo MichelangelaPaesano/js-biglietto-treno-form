6c429b78eb610ac3d72cc881ee6e8ce7ba1eae82
Michelangela Paesano <paesanomichelangela@gmail.com>
2025-05-22 13:40:03 +0200

    tolto alcuni spazi

--- js/app.js
+++ js/app.js
@@ -31,7 +31,6 @@ console.log (biglietto)
 //adesso dobbiamo solo cambiare il p con il risultato del biglietto 
 divRisultato.innerHTML = (`Il tuo viaggio avrà un costo di ${biglietto.toFixed(2)} EUR`) 
 
-
 })
 
 
